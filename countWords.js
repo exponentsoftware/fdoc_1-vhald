b7434d63b8585827f4e8be9066f257e4e932369a
vhald <vaibhavgsh@gmail.com>
2022-04-14 23:01:28 +0530

    attempt 1

---
+++ countWords.js
@@ -0,0 +1,15 @@
+// countWords and compare them frequency
+
+const paragraph = 'I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.';
+
+let w1 = "love";
+let w2 = "I";
+
+function countWords(para, word1, word2) {
+    if (word2 in para) {
+        let x = "this";
+    }
+    console.log(x);
+}
+
+countWords(paragraph, w1, w2);
